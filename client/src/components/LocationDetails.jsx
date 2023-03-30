f4dea1148c098368a63665425632305cd9bd4b8b
emilyphalperin@gmail.com <emilyhalperin@Emilys-MacBook-Pro-2.local>
2023-03-30 10:41:33 -0400

    pushed changes

--- client/src/components/LocationDetails.jsx
+++ client/src/components/LocationDetails.jsx
@@ -5,10 +5,10 @@ import placeholder from '../assets/placeholder.png'
 
 function LocationDetails() {
   return (
-    <div className='w-full bg-[#768781] py-20 px-6'>
+    <div id = "aqcontainer" className='w-full bg-[#768781] py-20 px-6'>
       <div className='max-w-[1240px] mx-auto py-1 px-5'>
-        <div className='flex justify-center'>
-          <h1 className='md:text-4xl sm:text-3xl text-2xl font-bold py-2 -mt-4 text-white'>AQ Data Generator</h1>
+        <div className='flex justify-center -mt-6'>
+          <h1 className='md:text-4xl sm:text-3xl text-2xl font-bold py-2 -mt-8 text-white'>AQ Data Generator</h1>
           <p></p>
         </div>
       {/* Implement logic for fetching air quality data and displaying it */}
